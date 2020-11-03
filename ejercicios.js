5894d857c6bbac7a500daf7fbd97995b4dcd9bd3
arbust908@gmail.com <fblanco@digitalhouse.com>
2020-11-02 23:44:22 -0300

    Exercises + links

---
+++ ejercicios.js
@@ -0,0 +1,16 @@
+Tablas Matematicas
+
+Hagamos una funcion que dado un numero nos de la tabla de ese numero, (del 1 al 10) en formato "x * 9 = 9x"
+
+Bonus: Usando esa funcion creemos otra funcion que tome dos numeros y nos devuelva las tablas de un numero al otro
+Tip: Para no repetir codigo podemos hacer una funcion que nos de la multiplicacion.
+
+El gran Asignador
+Con estos numeros hagamos una funcion que dado un numero te diga que nota te sacaste, en formato: "Con un x tu nota es un A"
+A de 100 a 90
+B de 90 a 80
+C de 80 a 70
+D de 70 a 60
+F el resto
+
+Bonus: Una funcion que nos debuelva una lista de evaluaciones dependiendo de la letra que le pasamos, en formato: "Con un x tu nota es un A"
